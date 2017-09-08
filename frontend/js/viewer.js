48e8cd87af5ddbca270f94cc6d684c61346199e3
Vincent Simard <vincent@vincentsimard.com>
2017-09-08 11:52:51 -0400

    Initial commit

---
+++ frontend/js/viewer.js
@@ -0,0 +1,7 @@
+(function() {
+  var menuBtn = document.getElementById('open-button');
+
+	menuBtn.addEventListener('click', function() {
+    document.body.classList.toggle('show-menu');
+  });
+})();
